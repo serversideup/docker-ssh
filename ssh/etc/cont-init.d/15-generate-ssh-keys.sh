4731cf421cd0b5cf5543cc192377d389f5818476
Jay Rogers <jay@521dimensions.com>
2021-08-25 17:38:57 -0500

    Create the SSH host key directory if it does not exist

--- ssh/etc/cont-init.d/15-generate-ssh-keys.sh
+++ ssh/etc/cont-init.d/15-generate-ssh-keys.sh
@@ -4,5 +4,9 @@
 if [ ! -f $SSH_HOST_KEY_DIR/ssh_host_* ]; then
   echo "🏃‍♂️ Generating SSH keys for you..."
   dpkg-reconfigure openssh-server
+  # Check if the host directory exists. Create it if needed
+  if [ ! -d $SSH_HOST_KEY_DIR ]; then
+    mkdir -p $SSH_HOST_KEY_DIR
+  fi
   find /etc/ssh/ -type f -name "ssh_host_*" -exec mv -t $SSH_HOST_KEY_DIR "{}" \;
 fi
