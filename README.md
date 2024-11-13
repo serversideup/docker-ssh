cf46d70e84c9d1f0f219085fd82bf15f1c5b14b1
Jay Rogers <jay@521dimensions.com>
2024-11-13 13:29:56 -0600

    Removed S6 and converted to Debian

--- README.md
+++ README.md
@@ -8,27 +8,6 @@
 	<a href="https://community.serversideup.net"><img alt="Discourse users" src="https://img.shields.io/discourse/users?color=blue&server=https%3A%2F%2Fcommunity.serversideup.net"></a>
   <a href="https://serversideup.net/discord"><img alt="Discord" src="https://img.shields.io/discord/910287105714954251?color=blueviolet"></a>
 </p>
-
-Hi! We're [Dan](https://twitter.com/danpastori) and [Jay](https://twitter.com/jaydrogers). We're a two person team with a passion for open source products. We created [Server Side Up](https://serversideup.net) to help share what we learn.
-
-### Find us at:
-
-* 📖 [Blog](https://serversideup.net) - get the latest guides and free courses on all things web/mobile development.
-* 🙋 [Community](https://community.serversideup.net) - get friendly help from our community members.
-* 🤵‍♂️ [Get Professional Help](https://serversideup.net/get-help) - get guaranteed responses within next business day.
-* 💻 [GitHub](https://github.com/serversideup) - check out our other open source projects
-* 📫 [Newsletter](https://serversideup.net/subscribe) - skip the algorithms and get quality content right to your inbox
-* 🐥 [Twitter](https://twitter.com/serversideup) - you can also follow [Dan](https://twitter.com/danpastori) and [Jay](https://twitter.com/jaydrogers)
-* ❤️ [Sponsor Us](https://github.com/sponsors/serversideup) - please consider sponsoring us so we can create more helpful resources
-
-### Our Sponsors
-All of our software is free an open to the world. None of this can be brought to you without the financial backing of our sponsors.
-
-<p align="center"><a href="https://github.com/sponsors/serversideup"><img src="https://521public.s3.amazonaws.com/serversideup/sponsors/sponsor-box.png" alt="Sponsors"></a></p>
-
-#### Individual Supporters
-<!-- supporters --><a href="https://github.com/deligoez"><img src="https://github.com/deligoez.png" width="40px" alt="deligoez" /></a>&nbsp;&nbsp;<a href="https://github.com/alexjustesen"><img src="https://github.com/alexjustesen.png" width="40px" alt="alexjustesen" /></a>&nbsp;&nbsp;<a href="https://github.com/jeremykenedy"><img src="https://github.com/jeremykenedy.png" width="40px" alt="jeremykenedy" /></a>&nbsp;&nbsp;<!-- supporters -->
-
 # About this project
 This is a super simple SSHD container based on Ubuntu 20.04. It works great if you need to create a secure tunnel into your cluster.
 
@@ -179,3 +158,54 @@ We'd love to have your help, but it might be best to explain your intentions fir
 
 ### Like we said -- we're always learning
 If you find a critical security flaw, please open an issue or learn more about [our responsible disclosure policy](https://www.notion.so/Responsible-Disclosure-Policy-421a6a3be1714d388ebbadba7eebbdc8).
+
+## Our Sponsors
+All of our software is free an open to the world. None of this can be brought to you without the financial backing of our sponsors.
+
+<p align="center"><a href="https://github.com/sponsors/serversideup"><img src="https://521public.s3.amazonaws.com/serversideup/sponsors/sponsor-box.png" alt="Sponsors"></a></p>
+
+### Black Level Sponsors
+<a href="https://sevalla.com"><img src="https://serversideup.net/wp-content/uploads/2024/10/sponsor-image.png" alt="Sevalla" width="546px"></a>
+
+#### Bronze Sponsors
+<!-- bronze -->No bronze sponsors yet. <a href="https://github.com/sponsors/serversideup">Become a sponsor →</a><!-- bronze -->
+
+#### Individual Supporters
+<!-- supporters --><a href="https://github.com/GeekDougle"><img src="https://github.com/GeekDougle.png" width="40px" alt="GeekDougle" /></a>&nbsp;&nbsp;<a href="https://github.com/JQuilty"><img src="https://github.com/JQuilty.png" width="40px" alt="JQuilty" /></a>&nbsp;&nbsp;<a href="https://github.com/MaltMethodDev"><img src="https://github.com/MaltMethodDev.png" width="40px" alt="MaltMethodDev" /></a>&nbsp;&nbsp;<!-- supporters -->
+
+## About Us
+We're [Dan](https://twitter.com/danpastori) and [Jay](https://twitter.com/jaydrogers) - a two person team with a passion for open source products. We created [Server Side Up](https://serversideup.net) to help share what we learn.
+
+<div align="center">
+
+| <div align="center">Dan Pastori</div>                  | <div align="center">Jay Rogers</div>                                 |
+| ----------------------------- | ------------------------------------------ |
+| <div align="center"><a href="https://twitter.com/danpastori"><img src="https://serversideup.net/wp-content/uploads/2023/08/dan.jpg" title="Dan Pastori" width="150px"></a><br /><a href="https://twitter.com/danpastori"><img src="https://serversideup.net/wp-content/themes/serversideup/images/open-source/twitter.svg" title="Twitter" width="24px"></a><a href="https://github.com/danpastori"><img src="https://serversideup.net/wp-content/themes/serversideup/images/open-source/github.svg" title="GitHub" width="24px"></a></div>                        | <div align="center"><a href="https://twitter.com/jaydrogers"><img src="https://serversideup.net/wp-content/uploads/2023/08/jay.jpg" title="Jay Rogers" width="150px"></a><br /><a href="https://twitter.com/jaydrogers"><img src="https://serversideup.net/wp-content/themes/serversideup/images/open-source/twitter.svg" title="Twitter" width="24px"></a><a href="https://github.com/jaydrogers"><img src="https://serversideup.net/wp-content/themes/serversideup/images/open-source/github.svg" title="GitHub" width="24px"></a></div>                                       |
+
+</div>
+
+### Find us at:
+
+* **📖 [Blog](https://serversideup.net)** - Get the latest guides and free courses on all things web/mobile development.
+* **🙋 [Community](https://community.serversideup.net)** - Get friendly help from our community members.
+* **🤵‍♂️ [Get Professional Help](https://serversideup.net/professional-support)** - Get video + screen-sharing support from the core contributors.
+* **💻 [GitHub](https://github.com/serversideup)** - Check out our other open source projects.
+* **📫 [Newsletter](https://serversideup.net/subscribe)** - Skip the algorithms and get quality content right to your inbox.
+* **🐥 [Twitter](https://twitter.com/serversideup)** - You can also follow [Dan](https://twitter.com/danpastori) and [Jay](https://twitter.com/jaydrogers).
+* **❤️ [Sponsor Us](https://github.com/sponsors/serversideup)** - Please consider sponsoring us so we can create more helpful resources.
+
+## Our products
+If you appreciate this project, be sure to check out our other projects.
+
+### 📚 Books
+- **[The Ultimate Guide to Building APIs & SPAs](https://serversideup.net/ultimate-guide-to-building-apis-and-spas-with-laravel-and-nuxt3/)**: Build web & mobile apps from the same codebase.
+- **[Building Multi-Platform Browser Extensions](https://serversideup.net/building-multi-platform-browser-extensions/)**: Ship extensions to all browsers from the same codebase.
+
+### 🛠️ Software-as-a-Service
+- **[Bugflow](https://bugflow.io/)**: Get visual bug reports directly in GitHub, GitLab, and more.
+- **[SelfHost Pro](https://selfhostpro.com/)**: Connect Stripe or Lemonsqueezy to a private docker registry for self-hosted apps.
+
+### 🌍 Open Source
+- **[AmplitudeJS](https://521dimensions.com/open-source/amplitudejs)**: Open-source HTML5 & JavaScript Web Audio Library.
+- **[Spin](https://serversideup.net/open-source/spin/)**: Laravel Sail alternative for running Docker from development → production.
+- **[Financial Freedom](https://github.com/serversideup/financial-freedom)**: Open source alternative to Mint, YNAB, & Monarch Money.
---
+++ src/rootfs/entrypoint.sh
@@ -0,0 +1,194 @@
+#!/bin/sh
+set -e
+default_uid='9999'
+default_gid='9999'
+default_unprivileged_user='tunnel'
+ssh_user=${SSH_USER:-"${default_unprivileged_user}"}
+ssh_host_key_dir=${SSH_HOST_KEY_DIR:-"/etc/ssh/ssh_host_keys"}
+ssh_user_home="/home/${ssh_user}"
+ssh_port=${SSH_PORT:-"2222"}
+
+
+if [ "$DEBUG" = "true" ]; then
+    set -x
+fi
+
+######################################################
+# Functions
+######################################################
+debug_print() {
+    if [ "$DEBUG" = "true" ]; then
+        echo "$1"
+    fi
+}
+
+######################################################
+# Main
+######################################################
+# Rename the Ansible user if it doesn't match the default
+if [ "$ssh_user" != "$default_unprivileged_user" ]; then
+
+    debug_print "Renaming user \"$default_unprivileged_user\" to \"$ssh_user\"..."
+
+    # Check if we're on Alpine or Debian
+    if [ -f /etc/alpine-release ] || [ -f /etc/debian_version ]; then
+        # Rename user and group
+        usermod -l "$ssh_user" "$default_unprivileged_user" || { echo "Failed to rename user"; exit 1; }
+        groupmod -n "$ssh_user" "$default_unprivileged_user" || { echo "Failed to rename group"; exit 1; }
+        
+        # Update home directory and move contents to new home directory
+        usermod -d "/home/$ssh_user" -m "$ssh_user" || { echo "Failed to update home directory"; exit 1; }
+        
+        if [ -f /etc/debian_version ]; then
+            # Update default group for Debian-based systems
+            usermod -g "$ssh_user" "$ssh_user" || { echo "Failed to update default group"; exit 1; }
+        fi
+        
+        debug_print "User and group renamed successfully. Home directory updated."
+    else
+        echo "Unsupported distribution for renaming user."
+        exit 1
+    fi
+fi
+
+# Change the SSH user and group to the specified UID and GID if they are not the default
+if { [ ! -z "${PUID}" ] && [ "${PUID}" != "$default_uid" ]; } || { [ ! -z "${PGID}" ] && [ "${PGID}" != "$default_gid" ]; }; then
+    debug_print "Preparing environment for $PUID:$PGID..."
+    
+    # Handle existing user with the same UID
+    if id -u "${PUID}" >/dev/null 2>&1; then
+        old_user=$(id -nu "${PUID}")
+        debug_print "UID ${PUID} already exists for user ${old_user}. Moving to a new UID."
+        usermod -u "999${PUID}" "${old_user}"
+    fi
+
+    # Handle existing group with the same GID
+    if getent group "${PGID}" >/dev/null 2>&1; then
+        old_group=$(getent group "${PGID}" | cut -d: -f1)
+        debug_print "GID ${PGID} already exists for group ${old_group}. Moving to a new GID."
+        groupmod -g "999${PGID}" "${old_group}"
+    fi
+
+    # Change UID and GID of ssh_user user and group
+    usermod -u "${PUID}" "${ssh_user}" 2>&1 >/dev/null || echo "Error changing user ID."
+    groupmod -g "${PGID}" "${ssh_user}" 2>&1 >/dev/null || echo "Error changing group ID."
+
+fi
+
+# Set SSHD configuration
+echo "🤖 Setting SSHD configuration..."
+{
+    echo "Port ${ssh_port}"
+    echo "PermitRootLogin no"
+    echo "DebianBanner no"
+    echo "PermitEmptyPasswords no"
+    echo "MaxAuthTries 5"
+    echo "LoginGraceTime 20"
+    echo "ChallengeResponseAuthentication no"
+    echo "KerberosAuthentication no"
+    echo "GSSAPIAuthentication no"
+    echo "X11Forwarding no"
+    echo "AllowAgentForwarding yes"
+    echo "AllowTcpForwarding yes"
+    echo "PermitTunnel yes"
+    echo "HostKey ${ssh_host_key_dir}/ssh_host_rsa_key"
+    echo "HostKey ${ssh_host_key_dir}/ssh_host_ecdsa_key"
+    echo "HostKey ${ssh_host_key_dir}/ssh_host_ed25519_key"
+} > /etc/ssh/sshd_config.d/custom.conf
+
+if [ "$DEBUG" = "true" ]; then
+    echo "🔥🔥🔥 Putting SSH server into Debug Mode..."
+    {
+        echo "SyslogFacility AUTHPRIV"
+        echo "LogLevel DEBUG"
+    } >> /etc/ssh/sshd_config.d/custom.conf
+fi
+
+# Check if SSH host keys are missing
+if [ ! -f "${ssh_host_key_dir}/ssh_host_rsa_key" ] || [ ! -f "${ssh_host_key_dir}/ssh_host_ecdsa_key" ] || [ ! -f "${ssh_host_key_dir}/ssh_host_ed25519_key" ]; then
+    echo "🏃‍♂️ Generating SSH keys for you..."
+    
+    # Create host key directory if it doesn't exist
+    mkdir -p "${ssh_host_key_dir}"
+    
+    # Generate the host keys directly
+    ssh-keygen -q -N "" -t rsa -f "${ssh_host_key_dir}/ssh_host_rsa_key"
+    ssh-keygen -q -N "" -t ecdsa -f "${ssh_host_key_dir}/ssh_host_ecdsa_key"
+    ssh-keygen -q -N "" -t ed25519 -f "${ssh_host_key_dir}/ssh_host_ed25519_key"
+    
+    # Set proper permissions
+    chmod 600 "${ssh_host_key_dir}"/ssh_host_*_key
+    chmod 644 "${ssh_host_key_dir}"/ssh_host_*_key.pub
+fi
+
+# Configure allowed IPs
+if [ -z "${ALLOWED_IPS}" ]; then
+    echo "🚨🚨🚨 CONFIGURATION ERROR:"
+    echo "ALLOWED_IPS environment variable is not set."
+    exit 1
+else
+    echo "📡 Setting allowed IPs (from ALLOWED_IPS variable) ..."
+    echo "${ALLOWED_IPS}" >> /etc/ssh/sshd_config.d/custom.conf
+fi
+
+# Setup authorized keys
+mkdir -p "${ssh_user_home}/.ssh/"
+
+if [ -n "$AUTHORIZED_KEYS" ] && [ ! -f "${ssh_user_home}/.ssh/authorized_keys" ]; then
+    echo "🔑 Setting authorized keys (from AUTHORIZED_KEYS variable)..."
+    echo "${AUTHORIZED_KEYS}" > "${ssh_user_home}/.ssh/authorized_keys"
+elif [ -z "$AUTHORIZED_KEYS" ] && [ -f /authorized_keys ]; then
+    echo "🔐 Using the provided authorized_keys file..."
+    cp /authorized_keys "${ssh_user_home}/.ssh/authorized_keys"
+elif [ -n "$AUTHORIZED_KEYS" ] && [ -f /authorized_keys ]; then
+    echo "⚠️ WARNING: Both AUTHORIZED_KEYS and authorized_keys file are set."
+    echo "ℹ️ INFO: We'll be using the AUTHORIZED_KEYS variable to configure SSH."
+    echo "${AUTHORIZED_KEYS}" > "${ssh_user_home}/.ssh/authorized_keys"
+else
+    echo "🚨🚨🚨 CONFIGURATION ERROR:"
+    echo "You must either set the AUTHORIZED_KEYS"
+    echo "environment variable or mount a configuration file to"
+    echo "${ssh_user_home}/.ssh/authorized_keys."
+    exit 1
+fi
+
+# Set proper permissions
+debug_print "Changing ownership of all files and directories..."
+chown "${PUID}:${PGID}" \
+    "${ssh_user_home}" \
+    "${ssh_host_key_dir}" \
+    "${ssh_user_home}/.ssh" \
+    "${ssh_user_home}/.ssh/authorized_keys"
+chmod 700 \
+    "${ssh_user_home}/.ssh" \
+    "${ssh_user_home}/.ssh/authorized_keys"
+
+# Create a custom MOTD
+echo "🎨 Creating custom MOTD..."
+{
+    echo '\033[38;5;75m╭──────────────────────────────────────────────────────╮\033[0m'
+    echo '\033[38;5;75m│                  🔐 SSH Tunnel Portal                |\033[0m'
+    echo '\033[38;5;75m╰──────────────────────────────────────────────────────╯\033[0m'
+    echo
+    echo '\033[1;36m📡 Connection Info:\033[0m'
+    echo "   • User: ${ssh_user}"
+    echo "   • Port: ${ssh_port}"
+    echo "   • Container IP: $(hostname -i)"
+    echo "   • Container ID: $(hostname)"
+    echo
+    echo '\033[1;35m🔒 Security:\033[0m'
+    echo "   • Allowed Users & IPs: ${ALLOWED_IPS}"
+    echo "   • Root Login: Disabled by default"
+    echo "   • Password Auth: Disabled by default"
+    echo
+    echo '\033[1;33m⚡ Need Help?\033[0m'
+    echo '   • Docs: https://github.com/serversideup/docker-ssh'
+    echo '   • Issues: https://github.com/serversideup/docker-ssh/issues'
+    echo '   • Community: https://serversideup.net/discord'
+    echo '   • Sponsor: https://github.com/sponsors/serversideup'
+    echo
+    echo '\033[38;5;242m─────────────────────────────────────────────────────────\033[0m'
+} > /etc/motd
+
+# Execute the CMD
+exec "$@"
